d5322026524412f07ef2680586e7fff3e2a9b9b5
DCalder98 <105236647+DCalder98@users.noreply.github.com>
2023-02-22 13:51:14 -0500

    Added Deployment web-service address

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
 
       // Save User to Database
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://landline.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -117,7 +117,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://landline.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
